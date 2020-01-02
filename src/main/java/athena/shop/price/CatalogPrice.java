b714f78d230e508d9235e139836383f0991ce591
Vrekt <vrekthf@gmail.com>
2020-01-02 00:08:45 -0600

    finish shop docs

--- src/main/java/athena/shop/price/CatalogPrice.java
+++ src/main/java/athena/shop/price/CatalogPrice.java
@@ -20,30 +20,51 @@ public final class CatalogPrice {
      */
     private Instant saleExpiration;
 
+    /**
+     * @return the currency type.
+     */
     public String currencyType() {
         return currencyType;
     }
 
+    /**
+     * @return the currency sub-type.
+     */
     public String currencySubType() {
         return currencySubType;
     }
 
+    /**
+     * @return the regular price.
+     */
     public int regularPrice() {
         return regularPrice;
     }
 
+    /**
+     * @return the dynamic regular price
+     */
     public int dynamicRegularPrice() {
         return dynamicRegularPrice;
     }
 
+    /**
+     * @return the final price.
+     */
     public int finalPrice() {
         return finalPrice;
     }
 
+    /**
+     * @return the base price.
+     */
     public int basePrice() {
         return basePrice;
     }
 
+    /**
+     * @return when the sale expires.
+     */
     public Instant saleExpiration() {
         return saleExpiration;
     }
--- src/main/java/athena/shop/requirement/CatalogEntryRequirement.java
+++ src/main/java/athena/shop/requirement/CatalogEntryRequirement.java
@@ -5,17 +5,32 @@ package athena.shop.requirement;
  */
 public final class CatalogEntryRequirement {
 
+    /**
+     * Requirement type and the ID.
+     */
     private String requirementType, requiredId;
+    /**
+     * Minimal quantity required.
+     */
     private int minQuantity;
 
+    /**
+     * @return the requirement-type.
+     */
     public String requirementType() {
         return requirementType;
     }
 
+    /**
+     * @return the required ID.
+     */
     public String requiredId() {
         return requiredId;
     }
 
+    /**
+     * @return the minimum quantity.
+     */
     public int minQuantity() {
         return minQuantity;
     }
